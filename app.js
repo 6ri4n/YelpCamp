9c6b97ca74a82950a5c307167f90f0a44bbaae3e
brian <bri4n.huang@gmail.com>
2023-04-29 18:15:25 -0700

    render deployment

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: true,
+    // secure: true, // causes an issue when deploying on render
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
